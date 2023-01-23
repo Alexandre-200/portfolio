62b5fcdb74b305ef6db906e00d57e4377c2ec5ea
Alexandre-200 <alexandre_200@outlook.com.br>
2023-01-23 19:09:34 -0300

    update

--- src/hooks/useNave.ts
+++ src/hooks/useNave.ts
@@ -3,19 +3,29 @@ import { NaveSides } from "./../types/naveSides";
 
 export const useNave = () => {
   const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [x,setX] = useState(0);
   const [side, setSide] = useState<NaveSides>("left");
 
-  useEffect(() => {
+  useEffect( () => {
     const w = window.innerWidth;
     const h = window.innerHeight;
+
+    setX(w/2);
+
     setPos((pos) => ({
+      
       x: w / 2,
       y: h - 80,
     }));
+   
   }, []);
 
   //canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
   const moveLeft = () => {
+
+    console.log(" x"+x);
+    console.log("pos y"+pos.y);
+    
     setPos((pos) => ({
       x: canMove(pos.x - 5, pos.y) ? pos.x - 5 : pos.x,
       y: pos.y,
